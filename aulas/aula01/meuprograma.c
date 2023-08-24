1e58e4c88e866dd4026324e07bbae1cff5b4e8f5
Gabriel8631 <128649049+Gabriel8631@users.noreply.github.com>
2023-08-24 00:52:33 +0000

    Arquivo aula 01

---
+++ aulas/aula01/meuprograma.c
@@ -0,0 +1,15 @@
+/* 
+  Autor: Gabriel Moraes
+  Data: 23/08/2023
+
+  Programa que exibe, olá Mundo!
+*/
+// função pricipal//
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+  printf("Olá Mundo!\n");
+  printf("A raiz quadrada de 2 é %f\n", sqrt(2));
+  return 0;
+}
---
+++ praticas/pratica01/questao01.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+  float nota_01 = 5.5f;
+  float nota_02 = 4.0f;
+
+  float media = 0.4 * nota_01 + 0.6 * nota_02;
+  
+  printf("A media é %f\n", media);
+
+  return 0;
+}
